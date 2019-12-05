6e3b42e710de891e0b3c3c86c93c90aa0b9619e9
Guillaume CAMANES <guillaume.camanes@niji.fr>
2019-12-05 09:29:15 +0100

    # add classical modules + eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    'global-require': 'off',
+    'import/no-unresolved': 'off',
+    'no-nested-ternary': 'off',
+    'no-param-reassign': 'off',
+    'react/forbid-prop-types': 'off',
+    'max-len': 'off',
+    'padded-blocks': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        'extensions': ['.js', '.jsx']
+      }
+    ],
+    'react/no-array-index-key': 'off',
+  }
 };
